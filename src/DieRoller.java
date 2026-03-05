9bb2e91d6032aa3e9a4a1d8d483d8d02846150e5
addisonevans <evans4a6@mail.uc.edu>
2026-03-05 16:18:44 -0500

    Lab 7 Complete!

--- src/DieRoller.java
+++ src/DieRoller.java
@@ -14,24 +14,40 @@ public class DieRoller {
         boolean playAgain = false;
         String answer = "";
 
-        System.out.println("Roll:      Die1  Die2  Die3  Sum");
-        System.out.println("________________________________");
-
 
         do {
 
-            while (!(die1 == die2 && die2 == die3)) {
-                rollNumber++;
-                die1 = rnd.nextInt(6) + 1;
-                die2 = rnd.nextInt(6) + 1;
-                die3 = rnd.nextInt(6) + 1;
+                die1 = 0;
+                die2 = 1;
+                die3 = 2;
+                rollNumber = 0;
 
-                int dieRoll = die1 + die2 + die3;
+                System.out.println("Roll:      Die1  Die2  Die3  Sum");
+                System.out.println("________________________________");
 
-                System.out.printf("Roll: %-4d %4d %4d %4d %4d\n", rollNumber, die1, die2, die3, dieRoll);
+                while (!(die1 == die2 && die2 == die3)) {
+                    rollNumber++;
+                    die1 = rnd.nextInt(6) + 1;
+                    die2 = rnd.nextInt(6) + 1;
+                    die3 = rnd.nextInt(6) + 1;
+
+                    int dieRoll = die1 + die2 + die3;
+
+                    System.out.printf("Roll: %-4d %4d %4d %4d %4d\n", rollNumber, die1, die2, die3, dieRoll);
+                }
+
+            System.out.print("\nTriple! Would you like to play again? [Y/N]: ");
+            answer = in.nextLine().toUpperCase();
+
+            if (answer.equals("Y"))
+            {
+                playAgain = false;
             }
-
-
+            else
+            {
+                System.out.println("\nThanks for playing!");
+                playAgain = true;
+            }
         }while (!playAgain);
     }
 }
